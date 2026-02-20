9adf10c080b2cadc0c7a4e2ce68c38123da4ef43
xboluda <xboludac@gmail.com>
2026-02-20 15:08:11 +0100

    Create projectprofit.lib.php

---
+++ projectprofit.lib.php
@@ -0,0 +1,336 @@
+<?php
+/* Copyright (C) 2026		Xavier Boluda				<xboludac@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    projectprofit/lib/projectprofit.lib.php
+ * \ingroup projectprofit
+ * \brief   Library files with common functions for ProjectProfit
+ */
+
+/**
+ * Prepare admin pages header
+ *
+ * @return array<array{string,string,string}>
+ */
+function projectprofitAdminPrepareHead()
+{
+	global $langs, $conf;
+
+	// global $db;
+	// $extrafields = new ExtraFields($db);
+	// $extrafields->fetch_name_optionals_label('myobject');
+
+	$langs->load("projectprofit@projectprofit");
+
+	$h = 0;
+	$head = array();
+
+	$head[$h][0] = dol_buildpath("/projectprofit/admin/setup.php", 1);
+	$head[$h][1] = $langs->trans("Settings");
+	$head[$h][2] = 'settings';
+	$h++;
+
+	/*
+	$head[$h][0] = dol_buildpath("/projectprofit/admin/myobject_extrafields.php", 1);
+	$head[$h][1] = $langs->trans("ExtraFields");
+	$nbExtrafields = (isset($extrafields->attributes['myobject']['label']) && is_countable($extrafields->attributes['myobject']['label'])) ? count($extrafields->attributes['myobject']['label']) : 0;
+	if ($nbExtrafields > 0) {
+		$head[$h][1] .= '<span class="badge marginleftonlyshort">' . $nbExtrafields . '</span>';
+	}
+	$head[$h][2] = 'myobject_extrafields';
+	$h++;
+
+	$head[$h][0] = dol_buildpath("/projectprofit/admin/myobjectline_extrafields.php", 1);
+	$head[$h][1] = $langs->trans("ExtraFieldsLines");
+	$nbExtrafields = (isset($extrafields->attributes['myobjectline']['label']) && is_countable($extrafields->attributes['myobjectline']['label'])) ? count($extrafields->attributes['myobject']['label']) : 0;
+	if ($nbExtrafields > 0) {
+		$head[$h][1] .= '<span class="badge marginleftonlyshort">' . $nbExtrafields . '</span>';
+	}
+	$head[$h][2] = 'myobject_extrafieldsline';
+	$h++;
+	*/
+
+	$head[$h][0] = dol_buildpath("/projectprofit/admin/about.php", 1);
+	$head[$h][1] = $langs->trans("About");
+	$head[$h][2] = 'about';
+	$h++;
+
+	// Show more tabs from modules
+	// Entries must be declared in modules descriptor with line
+	//$this->tabs = array(
+	//	'entity:+tabname:Title:@projectprofit:/projectprofit/mypage.php?id=__ID__'
+	//); // to add new tab
+	//$this->tabs = array(
+	//	'entity:-tabname:Title:@projectprofit:/projectprofit/mypage.php?id=__ID__'
+	//); // to remove a tab
+	complete_head_from_modules($conf, $langs, null, $head, $h, 'projectprofit@projectprofit');
+
+	complete_head_from_modules($conf, $langs, null, $head, $h, 'projectprofit@projectprofit', 'remove');
+
+	return $head;
+}
+
+function projectprofit_render_html($db, $data)
+{
+    require_once DOL_DOCUMENT_ROOT.'/projet/class/project.class.php';
+
+    $proj = new Project($db);
+
+    ob_start();
+
+    $hierarchy      = $data['hierarchy'];
+    $projects_info  = $data['projects_info'];
+    //$project_parent = $data['project_parent'];
+
+    $tot_ing = 0;
+    $tot_gas = 0;
+
+    ?>
+    <style>
+    .linea-header { cursor: pointer; }
+    </style>
+
+    <h3>Super Detail por Proyecto</h3>
+
+    <table border="1" cellpadding="3" cellspacing="0" width="100%">
+        <tr>
+            <th>Grupo</th>
+            <th>Inmueble</th>
+            <th>Tipo</th>
+            <th>Documento</th>
+            <th>Fecha</th>
+            <th>Tercero</th>
+            <th>Descripción</th>
+            <th>Cantidad</th>
+            <th>Total HT</th>
+            <th>Estado</th>
+        </tr>
+
+    <?php
+
+    // Pintar jerarquía padre -> hijo -> servicio -> lineas
+    foreach ($hierarchy as $padre_id => $hijos) {
+
+
+        // --- ASEGURAR INFO DEL PADRE REAL (aunque no haya filas del padre)
+        if (!isset($projects_info[$padre_id])) {
+
+            if ($proj->fetch($padre_id) > 0) {
+                $projects_info[$padre_id] = [
+                    'ref'   => $proj->ref,
+                    'title' => $proj->title
+                ];
+            } else {
+                $projects_info[$padre_id] = [
+                    'ref'   => $padre_id,
+                    'title' => 'Proyecto '.$padre_id
+                ];
+            }
+        }
+
+
+
+        $padre_toggle = 'padre_'.$padre_id;
+        $tot_padre_ing = 0;
+        $tot_padre_gas = 0;
+        $estado_padre = 'PAGADA'; // Inicializamos el estado del padre
+
+        // Primero calculamos el worst-case del padre recorriendo hijos y servicios
+        foreach ($hijos as $hijo_id => $servicios) {
+            $estado_hijo = 'PAGADA';
+            foreach ($servicios as $servicio_ref => $lineas) {
+                $estado_servicio = 'PAGADA';
+                foreach ($lineas as $l) {
+                    if ($l->estado_factura == 'VALIDADA') {
+                        $estado_servicio = 'VALIDADA';
+                        break;
+                    } elseif ($l->estado_factura == 'PARCIAL' && $estado_servicio != 'VALIDADA') {
+                        $estado_servicio = 'PARCIAL';
+                    }
+                }
+                // Propagar al hijo
+                if ($estado_servicio == 'VALIDADA') {
+                    $estado_hijo = 'VALIDADA';
+                } elseif ($estado_servicio == 'PARCIAL' && $estado_hijo != 'VALIDADA') {
+                    $estado_hijo = 'PARCIAL';
+                }
+            }
+            // Propagar al padre
+            if ($estado_hijo == 'VALIDADA') {
+                $estado_padre = 'VALIDADA';
+            } elseif ($estado_hijo == 'PARCIAL' && $estado_padre != 'VALIDADA') {
+                $estado_padre = 'PARCIAL';
+            }
+
+        }
+
+        // Color del padre según worst-case
+        $color_padre = '#27ae60';
+        if ($estado_padre == 'PARCIAL')  $color_padre = '#f39c12';
+        if ($estado_padre == 'VALIDADA') $color_padre = '#f44336';
+
+        // Fila PADRE
+        $label_padre = $projects_info[$padre_id]['title']; //?? 'Proyecto '.$padre_id;
+        $style = $forpdf ? '' : 'style="background:#d9edf7;font-weight:bold"';
+        $onclick = $forpdf ? '' : onclick="toggleClass(\'grp-'.$padre_toggle.'\')"';
+        echo '<tr class="linea-header" '.$style.' '.$onclick.'>';
+        echo '<td>'.$label_padre.'</td>';
+        echo '<td colspan="8"></td>';
+        echo '<td class="center">
+                <span title="'.$estado_padre.'"
+                    style="display:inline-block;width:10px;height:10px;border-radius:50%;background:'.$color_padre.'"></span>
+            </td>';
+        echo '</tr>';
+
+        foreach ($hijos as $hijo_id => $servicios) {
+
+            $hijo_toggle = 'hijo_'.$padre_id.'_'.$hijo_id;
+            $tot_hijo_ing = 0;
+            $tot_hijo_gas = 0;
+            $estado_hijo = 'PAGADA';
+
+            // Calcular worst-case del hijo
+            foreach ($servicios as $servicio_ref => $lineas) {
+                $estado_servicio = 'PAGADA';
+                foreach ($lineas as $l) {
+                    if ($l->estado_factura == 'VALIDADA') {
+                        $estado_servicio = 'VALIDADA';
+                        break;
+                    } elseif ($l->estado_factura == 'PARCIAL' && $estado_servicio != 'VALIDADA') {
+                        $estado_servicio = 'PARCIAL';
+                    }
+                }
+                if ($estado_servicio == 'VALIDADA') {
+                    $estado_hijo = 'VALIDADA';
+                } elseif ($estado_servicio == 'PARCIAL' && $estado_hijo != 'VALIDADA') {
+                    $estado_hijo = 'PARCIAL';
+                }
+            }
+
+            // Color del hijo
+            $color_hijo = '#27ae60';
+            if ($estado_hijo == 'PARCIAL')  $color_hijo = '#f39c12';
+            if ($estado_hijo == 'VALIDADA') $color_hijo = '#e74c3c';
+
+            // Fila HIJO
+	    $label_hijo  = $projects_info[$hijo_id]['title'] ?? 'Proyecto '.$hijo_id;
+            $style = $forpdf ? '' : 'style="display:none;background:#f7f7f7;font-weight:bold"';
+            $onclick = $forpdf ? '' : 'onclick="toggleClass(\'grp-'.$hijo_toggle.'\')"';
+            echo '<tr class="linea-header grp-'.$padre_toggle.'" '.$style.' '.$onclick.'>';
+            echo '<td></td>';
+            echo '<td>'.$label_hijo.'</td>';
+            echo '<td colspan="7"></td>';
+            echo '<td class="center">
+                    <span title="'.$estado_hijo.'"
+                        style="display:inline-block;width:10px;height:10px;border-radius:50%;background:'.$color_hijo.'"></span>
+                </td>';
+            echo '</tr>';
+
+            foreach ($servicios as $servicio_ref => $lineas) {
+
+                $serv_toggle = 'srv_'.$padre_id.'_'.$hijo_id.'_'.md5($servicio_ref);
+                $sub_ing = 0;
+                $sub_gas = 0;
+                $estado_servicio = 'PAGADA';
+
+                foreach ($lineas as $l) {
+                    if ($l->tipo_linea == 'INGRESO') $sub_ing += $l->total_ht;
+                    if ($l->tipo_linea == 'GASTO')   $sub_gas += $l->total_ht;
+
+                    if ($l->estado_factura == 'VALIDADA') {
+                        $estado_servicio = 'VALIDADA';
+                    } elseif ($l->estado_factura == 'PARCIAL' && $estado_servicio != 'VALIDADA') {
+                        $estado_servicio = 'PARCIAL';
+                    }
+
+                    // Totales finales
+                    if ($l->tipo_linea == 'INGRESO') $tot_ing += $l->total_ht;
+                    if ($l->tipo_linea == 'GASTO')   $tot_gas += $l->total_ht;
+                }
+
+                $tot_hijo_ing  += $sub_ing;
+                $tot_hijo_gas  += $sub_gas;
+
+                // Color del servicio
+                $color_srv = '#27ae60';
+                if ($estado_servicio == 'PARCIAL')  $color_srv = '#f39c12';
+                if ($estado_servicio == 'VALIDADA') $color_srv = '#f44336';
+
+                // Fila SERVICIO
+                $style = $forpdf ? '' : 'style="display:none"';
+                $onclick = $forpdf ? '' : 'onclick="toggleClass(\'grp-'.$serv_toggle.'\')"';
+                echo '<tr class="linea-header grp-'.$padre_toggle.' grp-'.$hijo_toggle.'" '.$style.' '.$onclick.'>';             
+                echo '<td></td><td></td>';
+                echo '<td>'.$servicio_ref.' - '.$lineas[0]->producto_nombre.'</td>';
+                echo '<td colspan="5"></td>';
+                echo '<td class="right">'.price($sub_ing - $sub_gas).'</td>';
+                echo '<td class="center">
+                        <span title="'.$estado_servicio.'" style="display:inline-block;width:10px;height:10px;border-radius:50%;background:'.$color_srv.'"></span>
+                    </td>';
+                echo '</tr>';
+
+                // Fila de líneas
+                foreach ($lineas as $l) {
+                    $color = '#999';
+                    if ($l->estado_factura == 'VALIDADA') $color = '#e74c3c';
+                    if ($l->estado_factura == 'PARCIAL')  $color = '#f39c12';
+                    if ($l->estado_factura == 'PAGADA')   $color = '#27ae60';
+
+                    echo '<tr class="grp-'.$padre_toggle.' grp-'.$hijo_toggle.' grp-'.$serv_toggle.'" style="display:none">';
+                    echo '<td></td><td></td><td>'.$l->tipo_linea.'</td>';
+                    echo '<td>'.$l->doc_ref.'</td>';
+                    echo '<td>'.dol_print_date($db->jdate($l->fecha),'day').'</td>';
+                    echo '<td>'.$l->tercero.'</td>';
+                    echo '<td>'.$l->descripcion.'</td>';
+                    echo '<td class="right">'.$l->qty.'</td>';
+                    echo '<td class="right">'.price($l->total_ht).'</td>';
+                    echo '<td class="center">
+                            <span title="'.$l->estado_factura.'" style="display:inline-block;width:10px;height:10px;border-radius:50%;background:'.$color.'"></span>
+                        </td>';
+                    echo '</tr>';
+                }
+
+            }
+
+            // subtotal HIJO
+            echo '<tr class="liste_total grp-'.$padre_toggle.'" style="display:none;background:#fcf8e3">';
+            echo '<td></td><td colspan="7" class="right">Subtotal proyecto '.$label_hijo.'</td>';
+            echo '<td class="right">'.price($tot_hijo_ing - $tot_hijo_gas).'</td>';
+            echo '<td></td>';
+            echo '</tr>';
+
+            $tot_padre_ing += $tot_hijo_ing;
+            $tot_padre_gas += $tot_hijo_gas;
+        }
+
+        // subtotal PADRE
+        echo '<tr class="liste_total" style="background:#dff0d8">';
+        echo '<td colspan="8" class="right">Subtotal proyecto padre '.$label_padre.'</td>';
+        echo '<td class="right">'.price($tot_padre_ing - $tot_padre_gas).'</td>';
+        echo '<td></td>';
+        echo '</tr>';
+    }
+
+
+    ?>
+
+    </table>
+
+    <?php
+
+    return ob_get_clean();
+}
